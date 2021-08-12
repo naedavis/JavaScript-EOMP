a47da278e57b4732c73b685897239fa459d431e7
Naeemah Davis <82158710+naedavis@users.noreply.github.com>
2021-08-12 16:31:18 +0200

    day 3 complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+
 function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
@@ -26,6 +27,31 @@ function login() {
     });
 }
 
-// function register(){
+function registration(){
+  const first_name = document.getElementById("first_name").value;
+  const last_name = document.getElementById("last_name").value;
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  console.log(first_name);
+  console.log(last_name);
+  console.log(username);
+  console.log(password);
+  fetch("https://flask-eomp.herokuapp.com/registration/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      first_name: first_name,
+      last_name: last_name,
+      username: username,
+      password: password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+     window.location.href = "login.html";
+    });
+}
 
-// }
+
